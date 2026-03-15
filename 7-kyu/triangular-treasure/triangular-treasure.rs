ff3dd9e7066c4e42954239e0949afe79a43da71f
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-03-15 21:18:08 +0530

    Add solution - CodeHub

---
+++ 7-kyu/triangular-treasure/triangular-treasure.rs
@@ -0,0 +1,14 @@
+fn triangular(n: i32) -> i32 
+{
+    // My solution: 1477 ms ( O(n) complexity)
+    if n <= 0 { return 0; }
+    (1..=n).sum()
+    
+    // The Fastest Way: 1346 ms ( O(1) Constant Time)
+//     if n <= 0 { return 0; }
+//     ((n * (n + 1)) / 2)
+​
+​
+​
+}
+​
