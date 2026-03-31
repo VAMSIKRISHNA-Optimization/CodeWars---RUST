17a5d5101c6fa52c2017af7f65f30d6681a0ccd7
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-03-31 22:00:12 +0530

    Add solution - CodeHub

---
+++ 3-kyu/prime-streaming-pg-13/prime-streaming-pg-13.rs
@@ -0,0 +1,28 @@
+//     }
+// }
+​
+// // Implementation of Iterator trait fir PrimeStreamIterator_1Million
+// impl Iterator for PrimeStreamIterator_1Million 
+// {
+//     type Item = u32;
+​
+//     fn next(&mut self) -> Option<Self::Item> 
+//     {
+//         if self.current_index < self.vec_of_primes.len() 
+//         {
+//             let prime_num = self.vec_of_primes[self.current_index];
+//             self.current_index += 1;
+//             Some(prime_num)
+//         } 
+//         else 
+//         {
+//             None 
+//         }
+//     }
+// }
+​
+// fn stream() -> impl Iterator<Item = u32> 
+// {
+//     PrimeStreamIterator_1Million::new(16_000_000) 
+// }
+​
