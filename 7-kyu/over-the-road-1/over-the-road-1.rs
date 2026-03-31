d3c342eca77123222f2f6f119edcfd9b352c714f
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-03-31 21:55:22 +0530

    Add solution - CodeHub

---
+++ 7-kyu/over-the-road-1/over-the-road-1.rs
@@ -0,0 +1,5 @@
+fn over_the_road(address: u64, n: u64) -> u64 
+{
+    /* My Solution - THE MOST EFFICIENT as well! (Time Complexity: O(1), Space Complexity: O(1)) : 1278 ms */
+    (n*2)-(address-1)      
+}
