c82d9742a4ccce2832a2dd788eccead268f98c41
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-04-01 22:49:12 +0530

    Add solution - CodeHub

---
+++ 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.rs
@@ -0,0 +1,11 @@
+fn to_alternating_case(s: &str) -> String 
+{
+  s.chars().map(|c| 
+      {
+          if c.is_lowercase()      { c.to_uppercase().to_string() }
+          else if c.is_uppercase() { c.to_lowercase().to_string() }
+          else { c.to_string() }
+      })
+    .collect::<Vec<String>>().
+    join("")
+}
