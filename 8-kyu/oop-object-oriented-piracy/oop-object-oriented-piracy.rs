6f69f631b81c0ed108a6712da41b84c16010ce14
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-04-01 21:58:57 +0530

    Add solution - CodeHub

---
+++ 8-kyu/oop-object-oriented-piracy/oop-object-oriented-piracy.rs
@@ -0,0 +1,15 @@
+struct Ship {
+  draft: u32,
+  crew: u32,
+}
+​
+impl Ship {
+    fn is_worth_it(&self) -> bool {
+        if self.draft as f32 - (self.crew as f32 * 1.5) > 20.0 {
+            true
+        }
+        else {
+            false
+        }
+    }
+}
