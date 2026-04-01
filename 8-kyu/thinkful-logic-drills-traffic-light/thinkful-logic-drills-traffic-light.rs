68873ce83f6b7da348ea438ba99e1d64dc91b2b2
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-04-01 22:40:12 +0530

    Add solution - CodeHub

---
+++ 8-kyu/thinkful-logic-drills-traffic-light/thinkful-logic-drills-traffic-light.rs
@@ -0,0 +1,8 @@
+fn update_light(current: &str) -> String {
+    match current {
+        "green" => "yellow".to_string(),
+        "yellow" => "red".to_string(),
+        "red" => "green".to_string(),
+        _ => "FAIL".to_string(),
+    }
+}
